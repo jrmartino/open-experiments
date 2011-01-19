409ce2212551f67dab0c69533b4125dec4b289a0
Ray Davis <ray@media.berkeley.edu>
2011-01-19 09:10:53 -0800

    Port the one sad unit test for the Delete Authorizable servlet - we should add some more in this sprint

--- bundles/user/src/test/java/org/sakaiproject/nakamura/user/servlet/DeleteSakaiAuthorizableServletTest.java
+++ bundles/user/src/test/java/org/sakaiproject/nakamura/user/servlet/DeleteSakaiAuthorizableServletTest.java
@@ -1,47 +1,50 @@
 package org.sakaiproject.nakamura.user.servlet;
 
-import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import org.apache.jackrabbit.api.JackrabbitSession;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.HtmlResponse;
 import org.apache.sling.servlets.post.Modification;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.sakaiproject.nakamura.api.lite.Session;
+import org.sakaiproject.nakamura.api.lite.SessionAdaptable;
 import org.sakaiproject.nakamura.api.user.AuthorizablePostProcessService;
-import org.sakaiproject.nakamura.testutils.easymock.AbstractEasyMockTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.jcr.Session;
+import javax.servlet.http.HttpServletResponse;
 
-public class DeleteSakaiAuthorizableServletTest extends AbstractEasyMockTest {
+public class DeleteSakaiAuthorizableServletTest {
 
   @Test
-  public void testHandleOperation() throws Exception {
-    DeleteSakaiAuthorizableServlet dsas = new DeleteSakaiAuthorizableServlet();
+  public void testApplyToAllowsNullResources() {
+    Session session = mock(Session.class);
+    javax.jcr.Session adaptable = mock(javax.jcr.Session.class, Mockito.withSettings().extraInterfaces(SessionAdaptable.class));
+    when(((SessionAdaptable)adaptable).getSession()).thenReturn(session);
+    SlingHttpServletRequest request = mock(SlingHttpServletRequest.class);
+    ResourceResolver resolver = mock(ResourceResolver.class);
+    when(request.getResourceResolver()).thenReturn(resolver);
+    when(resolver.adaptTo(javax.jcr.Session.class)).thenReturn(adaptable);
 
-    JackrabbitSession session = createMock(JackrabbitSession.class);
+    // This test checks that processing of a list of ":applyTo" IDs will continue
+    // without throwing an exception, even if the list is empty.
+    when(request.getParameterValues(":applyTo")).thenReturn(new String[] {});
+    when(request.getResource()).thenReturn(null);
 
-    ResourceResolver rr = createMock(ResourceResolver.class);
-    expect(rr.adaptTo(Session.class)).andReturn(session);
-
-    SlingHttpServletRequest request = createMock(SlingHttpServletRequest.class);
-    expect(request.getParameterValues(":applyTo")).andReturn(new String[] {}).times(2);
-    expect(request.getResourceResolver()).andReturn(rr).times(3);
-    expect(request.getResource()).andReturn(null).times(2);
-
+    AuthorizablePostProcessService authorizablePostProcessService = mock(AuthorizablePostProcessService.class);
     List<Modification> changes = new ArrayList<Modification>();
-
-    AuthorizablePostProcessService authorizablePostProcessService = createMock(AuthorizablePostProcessService.class);
-
     HtmlResponse response = new HtmlResponse();
-
-
-    replay();
+    DeleteSakaiAuthorizableServlet dsas = new DeleteSakaiAuthorizableServlet();
     dsas.postProcessorService = authorizablePostProcessService;
     dsas.handleOperation(request, response, changes);
-    verify();
+
+    // Nothing happened (since we didn't get an exception). Did an unwanted error get
+    // reported?
+    assertFalse(response.getStatusCode() == HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
   }
 }
