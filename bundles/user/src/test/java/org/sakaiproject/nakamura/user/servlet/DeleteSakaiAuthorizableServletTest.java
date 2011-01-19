b28281b4d16cd2feb427b988739c0f2da542fba7
Ray Davis <ray@media.berkeley.edu>
2011-01-19 10:29:30 -0800

    Bulk up the Delete Authorizable servlet unit tests; make some fixes

--- bundles/user/src/test/java/org/sakaiproject/nakamura/user/servlet/DeleteSakaiAuthorizableServletTest.java
+++ bundles/user/src/test/java/org/sakaiproject/nakamura/user/servlet/DeleteSakaiAuthorizableServletTest.java
@@ -1,17 +1,25 @@
 package org.sakaiproject.nakamura.user.servlet;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceNotFoundException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.HtmlResponse;
 import org.apache.sling.servlets.post.Modification;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.sakaiproject.nakamura.api.lite.Session;
 import org.sakaiproject.nakamura.api.lite.SessionAdaptable;
+import org.sakaiproject.nakamura.api.lite.authorizable.Authorizable;
 import org.sakaiproject.nakamura.api.user.AuthorizablePostProcessService;
 
 import java.util.ArrayList;
@@ -20,25 +28,42 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 public class DeleteSakaiAuthorizableServletTest {
+  @Mock
+  Session session;
+  @Mock
+  SlingHttpServletRequest request;
+  @Mock
+  ResourceResolver resourceResolver;
+  @Mock
+  AuthorizablePostProcessService authorizablePostProcessService;
+
+  // Can't mock with annotations because we need to mix in the SessionAdaptable interface.
+  javax.jcr.Session adaptableJcrSession;
+
+  List<Modification> changes;
+  HtmlResponse response;
+
+  public DeleteSakaiAuthorizableServletTest() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    javax.jcr.Session adaptable = mock(javax.jcr.Session.class, Mockito.withSettings().extraInterfaces(SessionAdaptable.class));
+    when(((SessionAdaptable)adaptable).getSession()).thenReturn(session);
+    when(request.getResourceResolver()).thenReturn(resourceResolver);
+    when(resourceResolver.adaptTo(javax.jcr.Session.class)).thenReturn(adaptable);
+    changes = new ArrayList<Modification>();
+    response = new HtmlResponse();
+  }
 
   @Test
   public void testApplyToAllowsNullResources() {
-    Session session = mock(Session.class);
-    javax.jcr.Session adaptable = mock(javax.jcr.Session.class, Mockito.withSettings().extraInterfaces(SessionAdaptable.class));
-    when(((SessionAdaptable)adaptable).getSession()).thenReturn(session);
-    SlingHttpServletRequest request = mock(SlingHttpServletRequest.class);
-    ResourceResolver resolver = mock(ResourceResolver.class);
-    when(request.getResourceResolver()).thenReturn(resolver);
-    when(resolver.adaptTo(javax.jcr.Session.class)).thenReturn(adaptable);
-
     // This test checks that processing of a list of ":applyTo" IDs will continue
     // without throwing an exception, even if the list is empty.
     when(request.getParameterValues(":applyTo")).thenReturn(new String[] {});
     when(request.getResource()).thenReturn(null);
 
-    AuthorizablePostProcessService authorizablePostProcessService = mock(AuthorizablePostProcessService.class);
-    List<Modification> changes = new ArrayList<Modification>();
-    HtmlResponse response = new HtmlResponse();
     DeleteSakaiAuthorizableServlet dsas = new DeleteSakaiAuthorizableServlet();
     dsas.postProcessorService = authorizablePostProcessService;
     dsas.handleOperation(request, response, changes);
@@ -46,5 +71,37 @@ public class DeleteSakaiAuthorizableServletTest {
     // Nothing happened (since we didn't get an exception). Did an unwanted error get
     // reported?
     assertFalse(response.getStatusCode() == HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+  }
+
+  @Test
+  public void testErrorForNullResource() {
+    when(request.getParameterValues(":applyTo")).thenReturn(null);
+    when(request.getResource()).thenReturn(null);
+
+    DeleteSakaiAuthorizableServlet dsas = new DeleteSakaiAuthorizableServlet();
+    dsas.postProcessorService = authorizablePostProcessService;
+    try {
+      dsas.handleOperation(request, response, changes);
+      fail("Null resource should result in ResourceNotFoundException");
+    } catch (ResourceNotFoundException e) {
+    }
+    assertEquals(HttpServletResponse.SC_NOT_FOUND, response.getStatusCode());
+  }
+
+  @Test
+  public void testErrorForUnfoundResource() {
+    Resource resource = mock(Resource.class);
+    when(resource.adaptTo(Authorizable.class)).thenReturn(null);
+    when(request.getParameterValues(":applyTo")).thenReturn(null);
+    when(request.getResource()).thenReturn(resource);
+
+    DeleteSakaiAuthorizableServlet dsas = new DeleteSakaiAuthorizableServlet();
+    dsas.postProcessorService = authorizablePostProcessService;
+    try {
+      dsas.handleOperation(request, response, changes);
+      fail("Null resource should result in ResourceNotFoundException");
+    } catch (ResourceNotFoundException e) {
+    }
+    assertEquals(HttpServletResponse.SC_NOT_FOUND, response.getStatusCode());
   }
 }
